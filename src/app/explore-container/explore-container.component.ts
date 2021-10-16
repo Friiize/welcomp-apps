44d348db08015e1a0bf5e5265d6593770881115e
Frize <ainsley.salty@gmail.com>
2021-10-16 20:17:29 +0200

    v1 test build and photo on device possible

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { createWorker } from 'tesseract.js';
 
 @Component({
   selector: 'app-explore-container',
@@ -7,9 +9,45 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ExploreContainerComponent implements OnInit {
   @Input() name: string;
+  outputText: string;
+  ocrResult = 'Recognizing...';
+  captureProgess = 0;
+  image;
+  worker;
+  constructor() {
+    this.doOCR();
+  }
 
-  constructor() { }
+  ngOnInit() {
+  }
 
-  ngOnInit() {}
+  async doOCR() {
+    const worker = createWorker({
+      logger: m => {
+        console.log(m);
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        (m.status === 'reconizing text') ? this.captureProgess = parseInt('' + m.progress * 100, 10): null;
+      },
+    });
+    await this.worker.load();
+    await this.worker.loadLanguage('eng');
+    await this.worker.initialize('eng');
+  };
 
+  async captureImage() {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: CameraResultType.Uri
+      });
+      console.log(image);
+      this.image = image.dataUrl;
+  }
+
+  async recognizeImage() {
+    const {data: {text}} = await this.worker.recognize(this.image);
+    this.ocrResult = text;
+    console.log(text);
+    await this.worker.terminate();
+  }
 }
