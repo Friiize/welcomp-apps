9cf7ba0aa45fc9377b027059936198462bdf9512
Raffaele Leruste <raffaele.leruste@gmail.com>
2022-03-06 16:19:48 +0100

    Refactoing code

--- src/app/facture-modeling/facture-modeling.component.ts
+++ src/app/facture-modeling/facture-modeling.component.ts
@@ -11,12 +11,6 @@ export class FactureModelingComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    /*const cameraPreviewOptions: CameraPreviewOptions = {
-    position: 'rear',
-    height: window.screen.height,
-    width: window.screen.width
-  };
-    CameraPreview.start(cameraPreviewOptions).then();*/
   }
 
 }
--- src/app/facture-recognition/facture-recognition.component.ts
+++ src/app/facture-recognition/facture-recognition.component.ts
@@ -11,28 +11,9 @@ import {Base64ConvertorService} from '../services/base64-convertor.service';
 export class FactureRecognitionComponent implements OnInit {
   @Input() name: string;
   outputText: string;
-  // ocrResult = 'Waiting for a picture...';
-  // captureProgress = 0;
-  // workerReady = false;
   image;
-  // worker: Tesseract.Worker;
-  constructor(private readonly factureService: FactureService, private readonly base64Convertor: Base64ConvertorService) {
-    // this.loadWorker().then();
-  }
+  constructor(private readonly factureService: FactureService, private readonly base64Convertor: Base64ConvertorService) {}
   ngOnInit() {}
-
-  // async loadWorker() {
-  //   this.ocrResult = 'Rechargement...';
-  //   this.worker = createWorker({
-  //     logger: progress => {
-  //       if (progress.status === 'reconizing text') {this.captureProgress = parseInt('' + progress.progress, 10);}
-  //     },
-  //   });
-  //   this.ocrResult = 'Prêt';
-  //   await this.worker.load();
-  //   await this.worker.loadLanguage('fra');
-  //   await this.worker.initialize('fra');
-  // };
 
   async captureImage() {
     const image = await Camera.getPhoto({
@@ -48,20 +29,8 @@ export class FactureRecognitionComponent implements OnInit {
     this.image = this.factureService.getData(formData).subscribe(data => {
       console.log(data);
     });
-
-    //this.image = this.factureService.test().subscribe();
-    //console.log(this.base64Convertor.convertToFile(image.dataUrl, 'factureImage'));
-    // this.workerReady = true;
   }
   async convertImage(image: Photo) {
     return this.base64Convertor.convertToFile((image.dataUrl), 'factureImage');
   }
-
-  // async recognizeImage() {
-  //   this.ocrResult = 'Recognizing...';
-  //   const result = await this.worker.recognize(this.image);
-  //   this.ocrResult = result.data.text;
-  //   console.log(this.ocrResult);
-  //   await this.worker.terminate();
-  // }
 }
